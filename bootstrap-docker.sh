2fe68f144d0cda0cd63d5cb54781fbbc69869109
Xiao Lin <linxiao9292@outlook.com>
2016-09-11 00:02:37 +0800

    Temp save for adding docker

---
+++ bootstrap-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# run the remaining commands as the vagrant user
+
+# install docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo touch /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get purge lxc-docker
+apt-cache policy docker-engine
+sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get -y install docker-engine
+sudo service docker start
+
+
+echo "FASTMaple VM booted successfully!"
