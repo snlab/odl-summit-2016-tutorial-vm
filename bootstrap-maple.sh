a1587f943b021d9fb95203c1004bd45757f90d70
jensenzhang <jingxuan.n.zhang@gmail.com>
2016-09-23 07:20:43 -0700

    Fix minor issue: rm -rf
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- bootstrap-maple.sh
+++ bootstrap-maple.sh
@@ -2,7 +2,7 @@
 
 su vagrant <<EOF
 
-rm /home/vagrant/tutorial/maple-archetype  
+rm -rf /home/vagrant/tutorial/maple-archetype
 pushd /home/vagrant/tutorial
 git clone https://github.com/snlab/maple-archetype.git
 
@@ -10,11 +10,11 @@ git clone https://github.com/snlab/maple-archetype.git
 pushd /home/vagrant/tutorial/maple-archetype
 mvn clean install -DskipTests
 popd
-rm -r /home/vagrant/tutorial/maple-archetype
+rm -rf /home/vagrant/tutorial/maple-archetype
 
 # Install Maple binary, *.tar.gz is reserved for staring a Maple from very clean stage
 curl -LO https://github.com/snlab/Maple-release/archive/1.0.0.tar.gz
-tar xvf 1.0.0.tar.gz 
+tar xvf 1.0.0.tar.gz
 # the name of decompress 1.0.0.tar.gz would be Maple-release-1.0.0
 mv 1.0.0.tar.gz Maple-release-1.0.0.tar.gz
 popd
