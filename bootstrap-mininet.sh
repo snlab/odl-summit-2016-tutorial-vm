713eb631eaad102bd45b4244921f55f4574b5d06
jensenzhang <jingxuan.n.zhang@gmail.com>
2016-09-26 08:28:34 -0700

    Add executable permission for start_maple_mininet
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- bootstrap-mininet.sh
+++ bootstrap-mininet.sh
@@ -9,4 +9,5 @@ fi
 sudo -u vagrant mkdir -p /home/vagrant/bin
 sudo cp /vagrant/utils/mininetSim /usr/local/bin/
 sudo echo 'sudo mn --controller=remote,ip=127.0.0.1 --mac --custom=/home/vagrant/Maple_Topo_Scripts/exampletopo.py --topo=mytopo --switch=ovs,protocols=OpenFlow13' > /usr/local/bin/start_maple_mininet
+sudo chmod a+x /usr/local/bin/start_maple_mininet
 echo "Mininet has been installed successfully!"
