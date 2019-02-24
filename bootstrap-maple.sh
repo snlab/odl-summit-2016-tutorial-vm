2ad72fba242083a1d359439bd019c58afe97693a
jensenzhang <jingxuan.n.zhang@gmail.com>
2019-02-24 12:57:18 -0500

    Fix #13: use latest maple relase installation script
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- bootstrap-maple.sh
+++ bootstrap-maple.sh
@@ -2,29 +2,11 @@
 
 su vagrant <<EOF
 
-#rm -rf /home/vagrant/tutorial/maple-archetype
-#pushd /home/vagrant/tutorial
-#git clone https://github.com/snlab/maple-archetype.git
-
-# Install Maple Archetype
-#pushd /home/vagrant/tutorial/maple-archetype
-#mvn clean install -DskipTests
-#popd
-#rm -rf /home/vagrant/tutorial/maple-archetype
-
 # Install Maple binary, *.tar.gz is reserved for staring a Maple from very clean stage
-pushd /home/vagrant/tutorial
-if [ -f "/home/vagrant/tutorial/maple-latest.zip" ]; then
-  rm -f maple-latest.zip
-fi
-curl -L -o maple-latest.zip $(curl -s https://api.github.com/repos/snlab/Maple-release/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
-if [ -d "/home/vagrant/tutorial/maple-latest" ]; then
-  rm -r maple-latest
-fi
-unzip maple-latest.zip
-popd
+bash <(curl https://raw.githubusercontent.com/snlab/Maple-release/master/utils/install.sh -L)
 
-cp /vagrant/utils/maple /home/vagrant/bin/maple
+curl -sSL https://raw.githubusercontent.com/snlab/Maple-release/master/utils/start_maple.sh -o /home/vagrant/bin/maple
+chmod +x /home/vagrant/bin/maple
 ln -s /home/vagrant/bin/maple /home/vagrant/bin/start_maple_controller
 
 echo "Maple App has been installed successfully!"
